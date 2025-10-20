e3ca5180a19a7ae2f4995463339abf24c30fe687
Lulizza <luizafioravanters@gmail.com>
2025-10-20 02:15:06 -0300

    criando tutorial da feature01

--- jackpotapp/backend/settings.py
+++ jackpotapp/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -47,9 +48,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
-ROOT_URLCONF = 'jackpotapp.urls'
+#durante dev
+CORS_ALLOW_ALL_ORIGINS = True
+
+ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'jackpotapp.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
